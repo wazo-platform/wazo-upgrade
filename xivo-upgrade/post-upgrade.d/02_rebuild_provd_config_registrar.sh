b9c15fb739be8fb9cff9214935b83be7e74c8ddd
Cedric Abunar <cabunar@avencall.com>
2012-03-28 14:14:39 -0400

    [fix]xivo-upgrade takes a long time to execute if no user is present #3147

--- xivo-upgrade/post-upgrade.d/02_rebuild_provd_config_registrar.sh
+++ xivo-upgrade/post-upgrade.d/02_rebuild_provd_config_registrar.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-wget -q http://localhost/xivo/configuration/json.php/private/provisioning/configregistrar/?act=rebuild_required_config -O - >/dev/null
+curl -s http://localhost/xivo/configuration/json.php/private/provisioning/configregistrar/?act=rebuild_required_config
