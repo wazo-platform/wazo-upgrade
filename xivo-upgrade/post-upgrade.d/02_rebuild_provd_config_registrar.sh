e6d7c8e9bafc0c41b46247e6eaea33813504f952
Etienne Lessard <elessard@avencall.com>
2012-04-05 13:26:52 -0400

    Force linefeatures num to 0 before rebuilding provd configs.

--- xivo-upgrade/post-upgrade.d/02_rebuild_provd_config_registrar.sh
+++ xivo-upgrade/post-upgrade.d/02_rebuild_provd_config_registrar.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+
+su - -c "psql -c 'update linefeatures set num = 1 where num = 0;' asterisk" postgres
 curl -s http://localhost/xivo/configuration/json.php/private/provisioning/configregistrar/?act=rebuild_required_config
