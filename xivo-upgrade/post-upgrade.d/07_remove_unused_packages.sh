c5b745a8e2452f2a3784c72eb89129b5a7c127c9
Nicolas HICHER (atarakt) <nhicher@proformatique.com>
2012-09-14 14:08:53 -0400

    [evo] add script to remove pf-xivo-web-interface-config

--- xivo-upgrade/post-upgrade.d/07_remove_unused_packages.sh
+++ xivo-upgrade/post-upgrade.d/07_remove_unused_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-old_fai=" pf-fai pf-fai-dev pf-fai-xivo-1.2-skaro pf-fai-xivo-1.2-skaro-dev"
+old_fais=" pf-fai pf-fai-dev pf-fai-xivo-1.2-skaro pf-fai-xivo-1.2-skaro-dev"
 renamed_packages="pf-xivo-agid"
 for package in $renamed_packages; do
     dpkg -l $package | grep -q '^ii'
@@ -7,4 +7,22 @@ for package in $renamed_packages; do
         apt-get purge $package
     fi
 done
-dpkg --purge $old_fai
+
+$all_packages+=$old_fais
+for old_fai in $all_packages; do
+    dpkg -l $package | grep -q '^rc'
+    if [ $? = 0 ]; then
+        dpkg --purge $old_fai
+    fi
+done
+
+
+extra="pf-xivo-web-interface"
+dpkg -l $extra | grep -q '^rc'
+if [ $? = 0 ]; then
+    rsync -av /etc/pf-xivo/web-interface /tmp/
+    dpkg --purge $extra
+    aptitude reinstall pf-xivo-webinterface
+    rsync -av /tmp/web-interface/ /etc/pf-xivo/web-interface/
+    rm -rf /tmp/web-interface
+fi
