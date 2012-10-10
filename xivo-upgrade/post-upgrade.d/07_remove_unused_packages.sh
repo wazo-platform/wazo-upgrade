acaf8a35334c5f2ae908fd8d2e2275e11de91176
Nicolas HICHER (atarakt) <nhicher@proformatique.com>
2012-10-10 11:28:01 -0400

    [evo] remove pf-xivo-fetchfw

--- xivo-upgrade/post-upgrade.d/07_remove_unused_packages.sh
+++ xivo-upgrade/post-upgrade.d/07_remove_unused_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 old_fais="pf-fai pf-fai-dev pf-fai-xivo-1.2-skaro pf-fai-xivo-1.2-skaro-dev"
-renamed_packages="pf-xivo-agid pf-xivo-base-config"
+renamed_packages="pf-xivo-agid pf-xivo-base-config pf-xivo-fetchfw"
 
 # cleanup pf-xivo-base-config.postrm file to allow package purge
 base_config_postrm="/var/lib/dpkg/info/pf-xivo-base-config.postrm"
