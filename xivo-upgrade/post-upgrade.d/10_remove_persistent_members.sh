7ee3d48a7ab8599b49af45edb5f7f1164c1da664
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-12-17 13:52:00 -0500

    Try to unload chan_agent.so and app_queue.so before removing the database entry

--- xivo-upgrade/post-upgrade.d/10_remove_persistent_members.sh
+++ xivo-upgrade/post-upgrade.d/10_remove_persistent_members.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+/usr/sbin/asterisk -rx "module unload chan_agent.so"
+/usr/sbin/asterisk -rx "module unload app_queue.so"
 /usr/sbin/asterisk -rx "database deltree Queue/PersistentMembers"
