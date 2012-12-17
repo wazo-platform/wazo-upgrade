3ed44acf6753c9147e58fc6694c27fe11b78c40e
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-12-17 14:15:57 -0500

    Does not work...

--- xivo-upgrade/post-upgrade.d/10_remove_persistent_members.sh
+++ xivo-upgrade/post-upgrade.d/10_remove_persistent_members.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-/usr/sbin/asterisk -rx "module unload chan_agent.so"
-/usr/sbin/asterisk -rx "module unload app_queue.so"
 /usr/sbin/asterisk -rx "database deltree Queue/PersistentMembers"
