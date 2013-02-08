34957b7de7c62132c1b6eda837ed904c664bcd02
Cedric Abunar <cabunar@avencall.com>
2013-02-08 10:38:40 -0500

    restore webi config file in first

--- xivo-upgrade/post-upgrade.d/01_update_webi.sh
+++ xivo-upgrade/post-upgrade.d/01_update_webi.sh
@@ -4,10 +4,6 @@ version=$(cat /usr/share/pf-xivo/XIVO-VERSION)
 webi_config_path='/etc/pf-xivo/web-interface'
 xivo_ini_path='/etc/pf-xivo/web-interface/xivo.ini'
 
-if [ -f "$xivo_ini_path" ]; then
-    sed -i -e 's/^language = fr/language = en/' -e 's/^territory = FR/territory = US/' "$xivo_ini_path"
-fi
-
 if [ $version \< '13.04' ]
 then
     for file in xivo ipbx cti; do
@@ -18,4 +14,8 @@ then
         touch /var/lib/pf-xivo/configured
         chmod 664 /var/lib/pf-xivo/configured
     fi
+fi
+
+if [ -f "$xivo_ini_path" ]; then
+    sed -i -e 's/^language = fr/language = en/' -e 's/^territory = FR/territory = US/' "$xivo_ini_path"
 fi
