b6d609e5ed26bd292ee6ffc812a65368f3297582
Cedric Abunar <cabunar@avencall.com>
2013-02-11 09:42:26 -0500

    do not execute rebuild provd config after each upgrade

--- xivo-upgrade/post-start.d/02_rebuild_provd_config_registrar.sh
+++ xivo-upgrade/post-start.d/02_rebuild_provd_config_registrar.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+is_executed_file="/var/lib/xivo-upgrade/$(basename $0)"
+
+if [ -f "$is_executed_file" ]; then
+    exit
+else
+    touch "$is_executed_file"
+fi
+
 su - -c "psql -c 'update linefeatures set num = 1 where num = 0;' asterisk > /dev/null" postgres
 curl -s http://localhost/xivo/configuration/json.php/private/provisioning/configregistrar/?act=rebuild_required_config
