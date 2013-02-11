cf703c3c93a17983694fc037b9b2a94582e866a2
Cedric Abunar <cabunar@avencall.com>
2013-02-11 07:41:15 -0500

    merge post-upgrade scripts

--- xivo-upgrade/post-upgrade.d/01_update_webi.sh
+++ xivo-upgrade/post-upgrade.d/01_update_webi.sh
@@ -3,6 +3,7 @@
 version=$(cat /usr/share/pf-xivo/XIVO-VERSION)
 webi_config_path='/etc/pf-xivo/web-interface'
 xivo_ini_path='/etc/pf-xivo/web-interface/xivo.ini'
+ipbx_config_file="/etc/pf-xivo/web-interface/ipbx.ini"
 
 if [ $version \< '13.04' ]; then
     for file in xivo ipbx cti; do
@@ -12,4 +13,8 @@ fi
 
 if [ -f "$xivo_ini_path" ]; then
     sed -i -e 's/^language = fr/language = en/' -e 's/^territory = FR/territory = US/' "$xivo_ini_path"
+fi
+
+if [ -f $ipbx_config_file ]; then
+    sed -i 's#/var/backups/pf-xivo#/var/backups/xivo#' $ipbx_config_file
 fi
--- xivo-upgrade/post-upgrade.d/08_update_config_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ipbx_config_file="/etc/pf-xivo/web-interface/ipbx.ini"
-
-if [ -f $ipbx_config_file ]; then
-    sed -i 's#/var/backups/pf-xivo#/var/backups/xivo#' $ipbx_config_file
-fi
-
