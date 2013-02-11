eb2ec7ecde01ce6c6bfc85acbe3075c46a9dfb0a
Cedric Abunar <cabunar@avencall.com>
2013-02-11 08:45:54 -0500

    move rename old file to package

--- xivo-upgrade/pre-start.d/01_update_webi.sh
+++ xivo-upgrade/pre-start.d/01_update_webi.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
 version=$(cat /usr/share/pf-xivo/XIVO-VERSION)
-webi_config_path='/etc/pf-xivo/web-interface'
 xivo_ini_path='/etc/pf-xivo/web-interface/xivo.ini'
 ipbx_config_file="/etc/pf-xivo/web-interface/ipbx.ini"
-
-if [ $version \< '13.04' ]; then
-    for file in xivo ipbx cti; do
-        mv ${webi_config_path}/${file}.ini.old ${webi_config_path}/${file}.ini
-    done
-fi
 
 if [ -f "$xivo_ini_path" ]; then
     sed -i -e 's/^language = fr/language = en/' -e 's/^territory = FR/territory = US/' "$xivo_ini_path"
