0a830e6bd097e7b4a9e4ddc9e91b1c6e6e1f4ab7
Cedric Abunar <cabunar@avencall.com>
2013-02-18 11:06:33 -0500

    fix restore webi config file after upgrade

--- xivo-upgrade/pre-stop.d/04_conf_handler.sh
+++ xivo-upgrade/pre-stop.d/04_conf_handler.sh
@@ -5,6 +5,6 @@ webi_config_path='/etc/pf-xivo/web-interface'
 
 if [ $version \< '13.04' ]; then
     for file in xivo ipbx cti; do
-        mv ${webi_config_path}/${file}.ini ${webi_config_path}/${file}.ini.old
+        mv ${webi_config_path}/${file}.ini ${webi_config_path}/${file}.ini.xivo-upgrade-old
     done
 fi
