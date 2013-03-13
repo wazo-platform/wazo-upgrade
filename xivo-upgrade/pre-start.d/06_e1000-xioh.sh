72f084228e3a6ae3d786220e1d65b5d9332b6992
Sebastien Duthil <sduthil@avencall.com>
2013-03-13 10:25:44 -0400

    fix identation

--- xivo-upgrade/pre-start.d/06_e1000-xioh.sh
+++ xivo-upgrade/pre-start.d/06_e1000-xioh.sh
@@ -5,7 +5,6 @@ for kernel in *; do
     old_module=$kernel/xioh/e1000.ko
     if [ -f $old_module ]; then
         rm $old_module
-	apt-get install e1000-xioh-modules-$kernel
+        apt-get install e1000-xioh-modules-$kernel
     fi
 done
-
