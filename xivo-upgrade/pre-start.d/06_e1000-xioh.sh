2c6b0462090b56aac08ea9bec88e78e49ba2992e
Noe Rubinstein <nrubinstein@avencall.com>
2013-03-26 16:12:20 +0100

    Install e1000-xioh-modules unconditionally
    Also avoid removing the old module if the install did not go well.

--- xivo-upgrade/pre-start.d/06_e1000-xioh.sh
+++ xivo-upgrade/pre-start.d/06_e1000-xioh.sh
@@ -4,7 +4,6 @@ cd /lib/modules
 for kernel in *; do
     old_module=$kernel/xioh/e1000.ko
     if [ -f $old_module ]; then
-        rm $old_module
-        apt-get install e1000-xioh-modules-$kernel
+        apt-get install -y e1000-xioh-modules-$kernel && rm $old_module
     fi
 done
