df11bd7d2be0f40b5155fd9ca3a6a4cdd7759178
Jean-Philippe Gelinas <jpgelinas@avencall.com>
2013-11-26 16:52:33 -0500

    better handle HA configuration update when upgrading PostgreSQL to 9.1

--- xivo-upgrade/post-start.d/update_config_ha.sh
+++ xivo-upgrade/post-start.d/update_config_ha.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ -f /etc/pf-xivo/ha.conf ]; then
+if [ -f /etc/pf-xivo/ha.conf -a -n "$UPGRADING_POSTGRESQL" ]; then
     echo "Updating configuration for HA..."
     curl -X POST -H "Content-Type: application/json" -d @/etc/pf-xivo/ha.conf http://localhost:8668/update_ha_config &>>/dev/null
+    xivo-service restart all
 fi
