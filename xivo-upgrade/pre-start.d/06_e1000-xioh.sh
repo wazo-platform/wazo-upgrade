8da4a0dbddd9ec1db27117db0b1bba4bb2d00fcf
Jean-Philippe Gelinas <jpgelinas@avencall.com>
2013-11-26 10:06:12 -0500

    remove e1000 module xioh related script

--- xivo-upgrade/pre-start.d/06_e1000-xioh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /lib/modules
-for kernel in *; do
-    old_module=$kernel/xioh/e1000.ko
-    if [ -f $old_module ]; then
-        apt-get install -y e1000-xioh-modules-$kernel && rm $old_module
-    fi
-done
