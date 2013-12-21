d5dce4a41263ddbdea0fb0eb6455f74dde59a795
Cedric Abunar <cabunar@avencall.com>
2013-12-21 13:19:24 -0500

    remove duplicate purge old fais already executed in real-xivo-upgrade

--- xivo-upgrade/pre-start.d/remove_unused_packages.sh
+++ xivo-upgrade/pre-start.d/remove_unused_packages.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-old_fais="pf-fai pf-fai-dev pf-fai-xivo-1.2-skaro pf-fai-xivo-1.2-skaro-dev"
 renamed_packages="pf-xivo-agid
 				pf-xivo-base-config
 				pf-xivo-fetchfw
@@ -46,13 +45,5 @@ for package in $renamed_packages; do
     dpkg -l $package 2> /dev/null | grep -q '^ii'
     if [ $? = 0 ]; then
         apt-get purge $package > /dev/null
-    fi
-done
-
-all_packages+="$renamed_packages $old_fais"
-for old_fai in $all_packages; do
-    dpkg -l $old_fai 2> /dev/null | grep -q '^rc'
-    if [ $? = 0 ]; then
-        dpkg --purge $old_fai > /dev/null
     fi
 done
