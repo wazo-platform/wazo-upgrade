910184106d7c07cb05a131ff5aec5b8b8608a55d
Cedric Abunar <cabunar@avencall.com>
2013-12-21 13:14:03 -0500

    let xivo-web-interface managing this own files

--- xivo-upgrade/pre-start.d/remove_unused_packages.sh
+++ xivo-upgrade/pre-start.d/remove_unused_packages.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 old_fais="pf-fai pf-fai-dev pf-fai-xivo-1.2-skaro pf-fai-xivo-1.2-skaro-dev"
-renamed_packages="pf-xivo-agid pf-xivo-base-config pf-xivo-fetchfw pf-xivo-web-interface-config pf-xivo-provd pf-xivo-dxtora pf-xivo-dhcpd-update pf-xivo-monitoring pf-xivo-web-interface pf-xivo-sysconfd"
-postrm_webi_config="/var/lib/dpkg/info/pf-xivo-web-interface-config.postrm"
+renamed_packages="pf-xivo-agid
+				pf-xivo-base-config
+				pf-xivo-fetchfw
+				pf-xivo-provd
+				pf-xivo-dxtora
+				pf-xivo-dhcpd-update
+				pf-xivo-monitoring
+				pf-xivo-web-interface
+				pf-xivo-web-interface-config
+				pf-xivo-sysconfd"
 
 echo "cleanup outdated config files"
-if [ -f $postrm_webi_config ]; then
-    rsync -av /etc/pf-xivo/web-interface /tmp/ > /dev/null
-    rm -rf $postrm_webi_config > /dev/null
-fi
 
 # cleanup pf-xivo-base-config.postrm file to allow package purge
 base_config_postrm="/var/lib/dpkg/info/pf-xivo-base-config.postrm"
 xivo_config_postrm="/var/lib/dpkg/info/xivo-config.postrm"
 if [ -f $base_config_postrm ]; then
-    rsync -av /etc/pf-xivo/web-interface /tmp/ > /dev/null
     cp $xivo_config_postrm $base_config_postrm
     sed -i 's/xivo-config/pf-xivo-base-config/' $base_config_postrm
 fi
@@ -31,7 +34,6 @@ old_provd_directory="/var/lib/pf-xivo-provd"
 if [ -d $old_provd_directory ]; then
     rsync -av $old_provd_directory/ /var/lib/xivo-provd/ > /dev/null
     rm -rf $old_provd_directory
-    /etc/init.d/xivo-provd restart
 fi
 
 # remove old postgresql-common and postgresql-client-common
@@ -40,14 +42,6 @@ if [ "$pg_common_version" = "140+0.xivo-backport-2" ]; then
     apt-get install -y --force-yes postgresql-common=134wheezy3~bpo60+1+xivo+1 postgresql-client-common=134wheezy3~bpo60+1+xivo+1
 fi
 
-old_web_interface_log_directory="/var/log/pf-xivo-web-interface"
-old_web_interface_config_directory="/etc/pf-xivo/web-interface"
-
-for dir in $old_web_interface_log_directory $old_web_interface_config_directory; do
-    if [ -d $dir ]; then
-        rm -rf $dir
-    fi
-done
 for package in $renamed_packages; do
     dpkg -l $package 2> /dev/null | grep -q '^ii'
     if [ $? = 0 ]; then
@@ -62,8 +56,3 @@ for old_fai in $all_packages; do
         dpkg --purge $old_fai > /dev/null
     fi
 done
-
-if [ -d /tmp/web-interface ]; then
-    rsync -av /tmp/web-interface/ /etc/pf-xivo/web-interface/ > /dev/null
-    rm -rf /tmp/web-interface > /dev/null
-fi
--- xivo-upgrade/pre-stop.d/04_conf_handler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-webi_config_path='/etc/xivo/web-interface'
-
-if [ $XIVO_VERSION_INSTALLED \< '13.04' ]; then
-    for file in xivo ipbx cti; do
-        mv ${webi_config_path}/${file}.ini ${webi_config_path}/${file}.ini.xivo-upgrade-old
-    done
-fi
