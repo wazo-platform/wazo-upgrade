295aca28c71bae9dc4096713b6c0a259670df90c
Sebastien Duthil <sduthil@avencall.com>
2014-07-18 10:53:50 -0400

    add provd migration script for unassociated devices
    Continuation of bug #4393. Devices that were in the wrong state
    (configured, but no line associated) before the upgrade would
    disappear from the webi after 14.13 (see commit in xivo-dao).

---
+++ post-start.d/20_reset_unassociated_devices_to_autoprov.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import logging
+
+from xivo_dao.helpers import provd_connector
+from xivo_dao.data_handler.line import services as lines
+from xivo_dao.data_handler.device import services as devices
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('reset_unassociated_devices_to_autoprov')
+
+logger.info('Fetching wrongly configured devices...')
+provd_device_manager = provd_connector.device_manager()
+
+configured_device_ids = {device['id']
+                         for device in provd_device_manager.find()
+                         if not device['config'].startswith('autoprov')}
+associated_device_ids = {line.device_id for line in lines.find_all()}
+
+wrongly_configured_device_ids = configured_device_ids - associated_device_ids
+
+logger.info('Resetting wrongly configured devices to autoprov...')
+for device_id in wrongly_configured_device_ids:
+    logger.info('Resetting device {}'.format(device_id))
+    try:
+        device = devices.get(device_id)
+        devices.reset_to_autoprov(device)
+    except Exception as e:
+        logging.exception(e)
+
+logger.info('Done.')
