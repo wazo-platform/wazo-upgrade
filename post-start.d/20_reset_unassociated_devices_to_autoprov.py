c697aa474e2ec56991243b599c05e480f971903b
Cedric Abunar <cabunar@avencall.com>
2014-07-18 11:47:54 -0400

    do not reset device to autoprov if device has no config key
    for example: creation of device via restapi does not create config key

--- post-start.d/20_reset_unassociated_devices_to_autoprov.py
+++ post-start.d/20_reset_unassociated_devices_to_autoprov.py
@@ -14,7 +14,7 @@ provd_device_manager = provd_connector.device_manager()
 
 configured_device_ids = {device['id']
                          for device in provd_device_manager.find()
-                         if not device['config'].startswith('autoprov')}
+                         if 'config' in device and not device['config'].startswith('autoprov')}
 associated_device_ids = {line.device_id for line in lines.find_all()}
 
 wrongly_configured_device_ids = configured_device_ids - associated_device_ids
