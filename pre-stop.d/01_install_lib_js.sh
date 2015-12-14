20cb8ec6cda9d6bac0c3cc6218ee3affd7de4649
Etienne Lessard <elessard@avencall.com>
2015-12-14 14:05:48 -0500

    xivo-lib-js* packages have been removed
    The javascript files are directly in the xivo-web-interface
    package now.

--- pre-stop.d/01_install_lib_js.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-dpkg -l pf-xivo-lib-js-jqplot 2>/dev/null | grep '^i' -q
-if [ $? -eq 0 ]; then
-    apt-get install -y xivo-lib-js-jqplot xivo-lib-js-jquery.mousewheel xivo-lib-js-schedule 1> /dev/null
-fi
