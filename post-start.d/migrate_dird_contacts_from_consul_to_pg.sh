d831679da41ff74e33952e367703f510f08ee959
Pascal Cadotte Michaud <pcadotte@proformatique.com>
2016-05-12 09:57:32 -0400

    consul to pg: fix escaped <>

--- post-start.d/migrate_dird_contacts_from_consul_to_pg.sh
+++ post-start.d/migrate_dird_contacts_from_consul_to_pg.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # There was no personal contacts before 15.13
-if [ "$XIVO_VERSION_INSTALLED" <\ "15.13" ]; then
+if [ "$XIVO_VERSION_INSTALLED" \< "15.13" ]; then
     exit 0
 fi
 
 # Personal contacts were stored in PG after version 16.05
-if [ "$XIVO_VERSION_INSTALLED" >\ "16.05" ]; then
+if [ "$XIVO_VERSION_INSTALLED" \> "16.05" ]; then
     exit 0
 fi
 
