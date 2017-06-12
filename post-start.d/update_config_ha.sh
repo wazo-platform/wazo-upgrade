406f3ed50b2e1b3b4a8af0a17945fc4e54214713
Sebastien Duthil <sduthil@wazo.community>
2017-06-12 10:41:15 -0400

    rename xivo-service to wazo-service

--- post-start.d/update_config_ha.sh
+++ post-start.d/update_config_ha.sh
@@ -3,5 +3,5 @@
 if [ -f /etc/xivo/ha.conf -a -n "$UPGRADING_POSTGRESQL" ]; then
     echo "Updating configuration for HA..."
     curl -X POST -H "Content-Type: application/json" -d @/etc/xivo/ha.conf http://localhost:8668/update_ha_config &>>/dev/null
-    xivo-service restart all
+    wazo-service restart all
 fi
