de3ff8862f89cf61585d5dffa35157199a42339f
Francois Blackburn <fblackburn@wazo.community>
2018-03-06 13:33:14 -0500

    set expire token to 10 hours
    reason: export of users can take a long long time

--- post-start.d/01_load_wazo_user_from_file.py
+++ post-start.d/01_load_wazo_user_from_file.py
@@ -58,7 +58,7 @@ def _create_user(auth_client, user):
 def _import_wazo_user(users):
     config = _load_config()
     auth_client = AuthClient(**config['auth'])
-    token = auth_client.token.new('xivo_service', expiration=300)['token']
+    token = auth_client.token.new('xivo_service', expiration=36000)['token']
     auth_client.set_token(token)
 
     print('migrate users to wazo-auth', end='', flush=True)
