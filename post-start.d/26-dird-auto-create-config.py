ce39b5f68569b94cad6cd95dbd67bddd444a2dba
Pascal Cadotte Michaud <pcm@wazo.io>
2019-04-05 14:32:20 -0400

    dird config: use http api to create default dird configs
    the old version published messages to the bus but this was not reliable because the bus connection was to
    slow to connect and the messages were missed.
    
    this new version uses the HTTP API and will be executed even if the old script has been executed
    if the personal source already exist for a tenant it quit.
    
    the office365 config is only generated if the plugin is installed and enabled

--- post-start.d/26-dird-auto-create-config.py
+++ post-start.d/26-dird-auto-create-config.py
@@ -5,12 +5,9 @@
 import argparse
 import os
 import sys
+import requests
 
-from kombu import (
-    Connection,
-    Exchange,
-    Producer,
-)
+from requests import HTTPError
 
 from xivo.chain_map import ChainMap
 from xivo.config_helper import (
@@ -18,12 +15,6 @@ from xivo.config_helper import (
     parse_config_file,
 )
 from xivo_auth_client import Client as AuthClient
-from xivo_bus import (
-    Marshaler,
-    Publisher,
-)
-from xivo_bus.resources.auth.events import TenantCreatedEvent
-from xivo_bus.resources.context.event import CreateContextEvent
 from xivo_confd_client import Client as ConfdClient
 
 _DEFAULT_CONFIG = {
@@ -32,6 +23,67 @@ _DEFAULT_CONFIG = {
     'auth': {
         'key_file': '/var/lib/wazo-auth-keys/wazo-upgrade-key.yml'
     }
+}
+
+DEFAULT_DISPLAY_COLUMNS = [
+    {'field': 'name', 'title': 'Nom', 'type': 'name'},
+    {'field': 'phone', 'title': "Num\xE9ro", 'type': 'number', 'number_display': '{name}'},
+    {'field': 'phone_mobile', 'title': 'Mobile', 'type': 'number', 'number_display': '{name} (mobile)'},
+    {'field': 'voicemail', 'title': "Bo\xEEte vocale", 'type': 'voicemail'},
+    {'field': 'favorite', 'title': 'Favoris', 'type': 'favorite'},
+    {'field': 'email', 'title': 'E-mail', 'type': 'email'},
+]
+PERSONAL_SOURCE_BODY = {
+    'name': 'personal',
+    'db_uri': 'postgresql://asterisk:proformatique@localhost/asterisk',
+    'format_columns': {
+        'phone': '{number}',
+        'name': '{firstname} {lastname}',
+        'phone_mobile': '{mobile}',
+        'reverse': '{firstname} {lastname}',
+    },
+    'searched_columns': ['firstname', 'lastname', 'number', 'mobile', 'fax'],
+    'first_matched_columns': ['number', 'mobile'],
+}
+WAZO_SOURCE_BODY = {
+    'auth': {
+        'host': 'localhost',
+        'port': 9497,
+        'verify_certificate': '/usr/share/xivo-certs/server.crt',
+        'key_file': '/var/lib/wazo-auth-keys/wazo-dird-wazo-backend-key.yml',
+        'version': '0.1',
+    },
+    'confd': {
+        'host': 'localhost',
+        'port': 9486,
+        'https': True,
+        'verify_certificate': '/usr/share/xivo-certs/server.crt',
+        'version': '1.1',
+    },
+    'format_columns': {
+        'phone': '{exten}',
+        'name': '{firstname} {lastname}',
+    },
+    'searched_columns': ['firstname', 'lastname', 'exten'],
+    'first_matched_columns': [],
+}
+OFFICE_365_SOURCE_BODY = {
+    'auth': {
+        'host': 'localhost',
+        'port': 9497,
+        'verify_certificate': '/usr/share/xivo-certs/server.crt',
+        'key_file': '/var/lib/wazo-auth-keys/wazo-dird-wazo-backend-key.yml',
+        'version': '0.1',
+    },
+    'endpoint': 'https://graph.microsoft.com/v1.0/me/contacts',
+    'format_columns': {
+        'name': '{givenName} {surname}',
+        'phone_mobile': '{mobilePhone}',
+        'reverse': '{givenName} {surname}',
+        'phone': '{businessPhones[0]}',
+    },
+    'searched_columns': ['givenName', 'surname', 'businessPhones'],
+    'first_matched_columns': ['mobilePhone', 'businessPhones'],
 }
 
 
@@ -57,47 +109,85 @@ def _auto_create_config():
     token = auth_client.token.new('wazo_user', expiration=36000)['token']
     auth_client.set_token(token)
     confd_client = ConfdClient(token=token, **config['confd'])
-    publisher = _make_publisher(config)
 
     tenants = auth_client.tenants.list()['items']
-    contexts = confd_client.contexts.list(recurse=True)['items']
 
+    base_url = 'https://{host}:{port}/{version}'.format(
+        **config['dird']
+    )
+    profiles_url = '{}/profiles'.format(base_url)
+    displays_url = '{}/displays'.format(base_url)
+    personal_url = '{}/backends/personal/sources'.format(base_url)
+    wazo_url = '{}/backends/wazo/sources'.format(base_url)
+    office365_url = '{}/backends/office365/sources'.format(base_url)
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-Auth-Token': token,
+    }
     for tenant in tenants:
-        _publish_tenant_created_event(publisher, **tenant)
-
-    for context in contexts:
-        if context['type'] != 'internal':
+        name = tenant['name']
+        if name == 'master':
             continue
-        _publish_context_created_event(publisher, context)
 
+        headers['Wazo-Tenant'] = tenant['uuid']
 
-def _publish_context_created_event(publisher, context):
-    event = CreateContextEvent(**context)
-    publisher.publish(event)
+        def post(*args, **kwargs):
+            return requests.post(
+                *args,
+                headers=headers,
+                verify=config['dird']['verify_certificate'],
+                **kwargs
+            )
 
+        personal_body = PERSONAL_SOURCE_BODY
+        personal = post(personal_url, json=personal_body)
+        if personal.status_code == 409:
+            # Default configuration already exists
+            return
 
-def _publish_tenant_created_event(publisher, uuid, name, **others):
-    event = TenantCreatedEvent(uuid, name)
-    publisher.publish(event)
+        wazo_body = dict(name='auto_wazo_{}'.format(name), **WAZO_SOURCE_BODY)
+        wazo = post(wazo_url, json=wazo_body)
 
+        sources = [personal.json(), wazo.json()]
 
-def _make_publisher(config):
-    bus_url = 'amqp://{username}:{password}@{host}:{port}//'.format(**config['bus'])
-    bus_connection = Connection(bus_url)
-    bus_exchange = Exchange(config['bus']['exchange_name'], type=config['bus']['exchange_type'])
-    bus_producer = Producer(bus_connection, exchange=bus_exchange, auto_declare=True)
-    bus_marshaler = Marshaler(config['uuid'])
-    return Publisher(bus_producer, bus_marshaler)
+        # add office365 is the plugin is installed
+        office365_body = dict(name='auto_office365_{}'.format(name), **OFFICE_365_SOURCE_BODY)
+        office365 = post(office365_url, json=office365_body)
+        if office365.status_code == 201:
+            sources.append(office365)
+
+        display_body = {
+            'name': 'auto_{}'.format(name),
+            'columns': DEFAULT_DISPLAY_COLUMNS,
+        }
+        display = post(displays_url, json=display_body).json()
+
+        contexts = confd_client.contexts.list(tenant_uuid=tenant['uuid'])['items']
+        for context in contexts:
+            if context['type'] != 'internal':
+                continue
+
+            profile_body = {
+                'name': context['name'],
+                'display': display,
+                'services': {
+                    'lookup': {'sources': sources},
+                    'favorites': {'sources': sources},
+                    'reverse': {'sources': sources, 'timeout': 0.5},
+                },
+            }
+            post(profiles_url, json=profile_body)
 
 
 def main():
     args = parse_args()
     if not args.force:
         version_installed = os.getenv('XIVO_VERSION_INSTALLED')
-        if version_installed >= '19.04':
+        if version_installed >= '19.06':
             sys.exit(0)
 
-    sentinel_file = '/var/lib/xivo-upgrade/dird-auto-create-config'
+    sentinel_file = '/var/lib/xivo-upgrade/dird-auto-create-config-v2'
     if os.path.exists(sentinel_file):
         # migration already done
         sys.exit(1)
