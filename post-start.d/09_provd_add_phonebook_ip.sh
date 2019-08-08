888e8d56ec2de4939ba7e9a605e969e767c8f172
Alexandre Fournier <afournier@wazo.io>
2019-08-08 11:44:08 -0400

    rename xivo-provd-cli to wazo-provd-cli

--- post-start.d/09_provd_add_phonebook_ip.sh
+++ post-start.d/09_provd_add_phonebook_ip.sh
@@ -2,4 +2,4 @@
 
 . /etc/xivo/common.conf
 
-xivo-provd-cli -c "configs['base'].set_config({'X_xivo_phonebook_ip': '$XIVO_NET4_IP'})" >/dev/null
+wazo-provd-cli -c "configs['base'].set_config({'X_xivo_phonebook_ip': '$XIVO_NET4_IP'})" >/dev/null
