8e9bebc8a250b10c62eb252940631c99105aa7ad
Sebastien Duthil <sduthil@wazo.community>
2019-09-11 19:14:51 -0400

    post-start: fix sentinel file path

--- post-start.d/57-provd-reconfigure-all-devices.py
+++ post-start.d/57-provd-reconfigure-all-devices.py
@@ -76,7 +76,7 @@ def main():
         if version_installed >= '19.13':
             sys.exit(0)
 
-    sentinel_file = '/var/lib/xivo-upgrade/57-provd-reconfigure-all-devices'
+    sentinel_file = '/var/lib/wazo-upgrade/57-provd-reconfigure-all-devices'
     if os.path.exists(sentinel_file):
         # migration already done
         sys.exit(1)
