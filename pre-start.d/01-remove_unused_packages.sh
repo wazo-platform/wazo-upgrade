46d0c73eab5346afe65122a51d2b8c3e5dd8a328
Alexandre Fournier <afournier@wazo.io>
2019-09-24 09:27:56 -0400

    purge xivo-dxtora

--- pre-start.d/01-remove_unused_packages.sh
+++ pre-start.d/01-remove_unused_packages.sh
@@ -29,7 +29,8 @@ renamed_packages="wazo-dird-phoned
                   xivo-agid
                   xivo-provd
                   xivo-provd-cli
-                  xivo-dhcpd-update"
+                  xivo-dhcpd-update
+                  xivo-dxtora"
 
 removed_packages="python-alembic
                   wazo-admin-ui
