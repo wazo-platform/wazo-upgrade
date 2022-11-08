d6811c99fd62684f8319d78034377549bbea4ab2
Jesse Sopel <jesse.sopel@wazo.io>
2022-11-08 08:36:05 -0500

    Also fix repo URL if it has been saved

--- post-start.d/60-replace-provd-py2-plugins.py
+++ post-start.d/60-replace-provd-py2-plugins.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # Copyright 2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
-
 import logging
 import os
 from time import sleep
@@ -44,7 +43,7 @@ def delete_cached_plugins():
             os.remove(file_path)
 
 
-def wait_until_completed(oip: OperationInProgress, wait = 5):
+def wait_until_completed(oip: OperationInProgress, wait=5):
     if oip.state == OIP_SUCCESS:
         return True
     if oip.state == OIP_FAIL or wait <= 0:
@@ -55,27 +54,43 @@ def wait_until_completed(oip: OperationInProgress, wait = 5):
     return wait_until_completed(oip, wait)
 
 
-def main():
-    config = load_config()
-    auth_client = AuthClient(**config['auth'])
-    token_data = auth_client.token.new(expiration=300)
-    provd_client = ProvdClient(token=token_data['token'], **config['provd'])
+def update_plugin_repo_url(client: ProvdClient):
+    current_url = client.params.get('plugin_server')['value']
+    if current_url and current_url.startswith('http://provd.wazo.community/plugins/1/'):
+        new_url = current_url.replace(
+            'http://provd.wazo.community/plugins/1/',
+            'http://provd.wazo.community/plugins/2/',
+        )
+        client.params.update('plugin_server', new_url)
 
-    installed_plugins = list(provd_client.plugins.list()['plugins'])
-    delete_cached_plugins()
+
+def remove_and_reinstall_plugins(client: ProvdClient):
+    installed_plugins = list(client.plugins.list()['plugins'])
 
     # remove old plugins
     for plugin in installed_plugins:
-        provd_client.plugins.uninstall(plugin)
+        client.plugins.uninstall(plugin)
 
     # Update plugin registry
-    wait_until_completed(provd_client.plugins.update())
+    wait_until_completed(client.plugins.update())
 
     # Rename old xivo plugins with the wazo named ones and use a set to avoid duplicates since some were combined
     plugins_to_install = {p.replace('xivo-', 'wazo-') for p in installed_plugins}
 
     for plugin in plugins_to_install:
-        provd_client.plugins.install(plugin)
+        client.plugins.install(plugin)
+
+
+def main():
+    config = load_config()
+    provd_config = config['provd']
+    auth_client = AuthClient(**config['auth'])
+    token_data = auth_client.token.new(expiration=300)
+    provd_client = ProvdClient(token=token_data['token'], **provd_config)
+
+    delete_cached_plugins()
+    update_plugin_repo_url(provd_client)
+    remove_and_reinstall_plugins(provd_client)
 
 
 if __name__ == '__main__':
