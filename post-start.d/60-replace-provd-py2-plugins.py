61b5ea3dee4f3be13c44ff99a054d73288f454aa
Jesse Sopel <jesse.sopel@wazo.io>
2022-11-16 09:56:09 -0500

    Also check https, abort on failures and only install installable plugins, use upgrade instead of uninstall/install

--- post-start.d/60-replace-provd-py2-plugins.py
+++ post-start.d/60-replace-provd-py2-plugins.py
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 import logging
 import os
+import re
+import shutil
 import sys
 from time import sleep
 
@@ -22,7 +24,9 @@ _DEFAULT_CONFIG = {
         'key_file': '/var/lib/wazo-auth-keys/wazo-upgrade-key.yml'
     }
 }
-PLUGIN_CACHE_DIR = '/var/cache/wazo-provd/'
+REPO_REGEX = re.compile(r'^(https?)://provd.wazo.community/plugins/1/(testing|stable|archive)/$')
+DEFAULT_PLUGIN_CACHE_DIR = '/var/cache/wazo-provd/'
+DEFAULT_PLUGIN_DIR = '/var/lib/wazo-provd/plugins/'
 SENTINEL_FILE = '/var/lib/wazo-upgrade/replace-provd-py2-plugins'
 
 
@@ -37,50 +41,68 @@ def load_config():
     return ChainMap(key_config, file_config, _DEFAULT_CONFIG)
 
 
-def delete_cached_plugins():
+def delete_cached_plugins(cache_dir):
     """Delete plugin cache to ensure provd does not re-use python2 plugins."""
-    for plugin in os.listdir(PLUGIN_CACHE_DIR):
-        file_path = os.path.join(PLUGIN_CACHE_DIR, plugin)
+    for plugin in os.listdir(cache_dir):
+        file_path = os.path.join(cache_dir, plugin)
         if os.path.isfile(file_path):
             os.remove(file_path)
 
 
-def wait_until_completed(oip: OperationInProgress, wait=5):
+def rename_xivo_plugins(plugins, plugin_dir):
+    for old_plugin_name in plugins:
+        new_plugin_name = old_plugin_name.replace('xivo-', 'wazo-')
+        shutil.move(os.path.join(plugin_dir, old_plugin_name), os.path.join(plugin_dir, new_plugin_name))
+
+
+def wait_until_completed(oip: OperationInProgress, wait=30):
     if oip.state == OIP_SUCCESS:
         return True
     if oip.state == OIP_FAIL or wait <= 0:
         return False
     oip.update()
     wait -= 1
-    sleep(.5)
+    sleep(1)
     return wait_until_completed(oip, wait)
 
 
 def update_plugin_repo_url(client: ProvdClient):
     current_url = client.params.get('plugin_server')['value']
-    if current_url and current_url.startswith('http://provd.wazo.community/plugins/1/'):
-        new_url = current_url.replace(
-            'http://provd.wazo.community/plugins/1/',
-            'http://provd.wazo.community/plugins/2/',
-        )
-        client.params.update('plugin_server', new_url)
+    if not current_url:
+        return
+    match = REPO_REGEX.match(current_url)
+    if match:
+        proto, variant = match.groups()
+        client.params.update('plugin_server', f'{proto}://provd.wazo.community/plugins/2/{variant}/')
 
 
-def remove_and_reinstall_plugins(client: ProvdClient):
-    installed_plugins = list(client.plugins.list()['plugins'])
+def remove_and_reinstall_plugins(client: ProvdClient, plugin_dir: str):
+    # Get all installed plugins that are still installable (i.e. skip old packages that are no longer available)
+    installable_plugins = list(client.plugins.list_installable()['pkgs'])
+    plugins = [p for p in client.plugins.list_installed()['pkgs'] if p in installable_plugins]
+    xivo_plugins = [p for p in plugins if p.startswith('xivo-')]
 
-    # remove old plugins
-    for plugin in installed_plugins:
-        client.plugins.uninstall(plugin)
+    # Rename old plugins if it exists
+    rename_xivo_plugins(xivo_plugins, plugin_dir)
 
     # Update plugin registry
-    wait_until_completed(client.plugins.update())
+    if wait_until_completed(client.plugins.update()) is False:
+        print('Unable to update repository', file=sys.stderr)
+        sys.exit(1)
 
-    # Rename old xivo plugins with the wazo named ones and use a set to avoid duplicates since some were combined
-    plugins_to_install = {p.replace('xivo-', 'wazo-') for p in installed_plugins}
+    # remove old plugins
+    for plugin in plugins:
+        plugin_name = plugin.replace('xivo-', 'wazo-')
+        if wait_until_completed(client.plugins.upgrade(plugin_name)) is False:
+            print(f'Failed to install plugin {plugin_name}', file=sys.stderr)
+            sys.exit(1)
 
-    for plugin in plugins_to_install:
-        client.plugins.install(plugin)
+    # Update devices that were linked to old plugin names
+    if xivo_plugins:
+        for device in client.devices.list()['devices']:
+            if device['plugin'] in xivo_plugins:
+                device['plugin'] = device['plugin'].replace('xivo-', 'wazo-')
+                client.devices.update(device)
 
 
 def main():
@@ -92,10 +114,13 @@ def main():
     auth_client = AuthClient(**config['auth'])
     token_data = auth_client.token.new(expiration=300)
     provd_client = ProvdClient(token=token_data['token'], **provd_config)
+    general_config = provd_config.get('general', {})
+    plugin_dir = general_config.get('base_storage_dir', DEFAULT_PLUGIN_DIR)
+    cache_dir = general_config.get('cache_dir', DEFAULT_PLUGIN_CACHE_DIR)
 
-    delete_cached_plugins()
+    delete_cached_plugins(cache_dir)
     update_plugin_repo_url(provd_client)
-    remove_and_reinstall_plugins(provd_client)
+    remove_and_reinstall_plugins(provd_client, plugin_dir)
 
     with open(SENTINEL_FILE, 'w'):
         pass
