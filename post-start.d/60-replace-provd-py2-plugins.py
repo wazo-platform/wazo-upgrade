7bdd80736765cb09db1a277ef816911360d269e1
Jesse Sopel <jesse.sopel@wazo.io>
2022-11-18 15:13:38 -0500

    Use os.rename and handle case where both xivo- and wazo- plugins were installed

--- post-start.d/60-replace-provd-py2-plugins.py
+++ post-start.d/60-replace-provd-py2-plugins.py
@@ -52,7 +52,21 @@ def delete_cached_plugins(cache_dir):
 def rename_xivo_plugins(plugins, plugin_dir):
     for old_plugin_name in plugins:
         new_plugin_name = old_plugin_name.replace('xivo-', 'wazo-')
-        shutil.move(os.path.join(plugin_dir, old_plugin_name), os.path.join(plugin_dir, new_plugin_name))
+        source_dir = os.path.join(plugin_dir, old_plugin_name)
+        target_dir = os.path.join(plugin_dir, new_plugin_name)
+        if os.path.exists(source_dir):
+            # If there was both a wazo- and a xivo- version copy the contents from the xivo one
+            for file_name in os.listdir(source_dir):
+                if file_name == 'build.py':
+                    # Skip this file, it will be replaced anyway on upgrade
+                    continue
+                if file_name == 'common' and old_plugin_name == 'xivo-gigaset':
+                    file_name = 'common-c'
+                    os.rename(os.path.join(source_dir, 'common'), os.path.join(source_dir, 'common-c'))
+                shutil.move(os.path.join(source_dir, file_name), target_dir)
+            shutil.rmtree(source_dir)
+        else:
+            os.rename(source_dir, target_dir)
 
 
 def wait_until_completed(oip: OperationInProgress, wait=30):
