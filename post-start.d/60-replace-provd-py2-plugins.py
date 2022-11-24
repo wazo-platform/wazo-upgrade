01c80f2b6b6c918cd94e4243de3af99394b19e1a
Jesse Sopel <jesse.sopel@wazo.io>
2022-11-24 10:32:33 -0500

    Fix conditions for moving/renaming plugins

--- post-start.d/60-replace-provd-py2-plugins.py
+++ post-start.d/60-replace-provd-py2-plugins.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 import logging
 import os
+from os import path
 import re
 import shutil
 import sys
@@ -45,30 +46,29 @@ def load_config():
 def delete_cached_plugins(cache_dir):
     """Delete plugin cache to ensure provd does not re-use python2 plugins."""
     for plugin in os.listdir(cache_dir):
-        file_path = os.path.join(cache_dir, plugin)
-        if os.path.isfile(file_path):
+        file_path = path.join(cache_dir, plugin)
+        if path.isfile(file_path):
             os.remove(file_path)
 
 
 def rename_xivo_plugins(plugins, plugin_dir):
     for old_plugin_name in plugins:
         new_plugin_name = old_plugin_name.replace('xivo-', 'wazo-')
-        source_dir = os.path.join(plugin_dir, old_plugin_name)
-        target_dir = os.path.join(plugin_dir, new_plugin_name)
-        if os.path.exists(source_dir):
+        source_dir = path.join(plugin_dir, old_plugin_name)
+        target_dir = path.join(plugin_dir, new_plugin_name)
+        if path.exists(target_dir):
             # If there was both a wazo- and a xivo- version copy the contents from the xivo one
             for file_name in os.listdir(source_dir):
-                if file_name == 'build.py':
-                    # Skip this file, it will be replaced anyway on upgrade
-                    continue
                 if file_name == 'common' and old_plugin_name == 'xivo-gigaset':
                     file_name = 'common-c'
-                    os.rename(os.path.join(source_dir, 'common'), os.path.join(source_dir, 'common-c'))
-                source = os.path.join(source_dir, file_name)
-                target = os.path.join(target_dir, file_name)
-                if os.path.isdir(source) and os.path.exists(target) and os.path.isdir(target):
+                    os.rename(path.join(source_dir, 'common'), path.join(source_dir, 'common-c'))
+                source = path.join(source_dir, file_name)
+                target = path.join(target_dir, file_name)
+                if path.isdir(source) and path.exists(target) and path.isdir(target):
+                    # Copy directory and its contents
                     copy_tree(source, target)
-                elif not os.path.isdir(target):
+                elif path.isfile(target):
+                    # No point overriding files at root
                     continue
                 else:
                     shutil.move(source, target_dir)
@@ -129,7 +129,7 @@ def remove_and_reinstall_plugins(client: ProvdClient, plugin_dir: str):
 
 
 def main():
-    if os.path.exists(SENTINEL_FILE):
+    if path.exists(SENTINEL_FILE):
         sys.exit(0)
 
     config = load_config()
