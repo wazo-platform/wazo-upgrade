9fb36669e2f4501fbf269755ed8c4ce81709ccba
Charles Langlois <charles.langlois@wazo.io>
2023-03-28 10:28:50 -0400

    Added upgrade sentinel file for install-ha-sentinel upgrade task

--- post-start.d/65-install-ha-sentinel.sh
+++ post-start.d/65-install-ha-sentinel.sh
@@ -7,11 +7,18 @@ set -u
 set -o pipefail
 
 HA_CONF=/etc/xivo/ha.conf
-if [ -f "$HA_CONF" ]; then
+UPGRADE_SENTINEL=/var/lib/wazo-upgrade/install-ha-sentinel
+if [ -f "$UPGRADE_SENTINEL" ]; then
+    exit 0
+elif [ -f "$HA_CONF" ]; then
     node_type=$(jq -r '.node_type' $HA_CONF)
     if [ "$node_type" = "slave" ]; then
+        rm -f /var/lib/wazo/is-primary
         touch /var/lib/wazo/is-secondary
     elif [ "$node_type" = "master" ]; then
+        rm -f /var/lib/wazo/is-secondary
         touch /var/lib/wazo/is-primary
     fi
 fi
+touch "$UPGRADE_SENTINEL"
+
