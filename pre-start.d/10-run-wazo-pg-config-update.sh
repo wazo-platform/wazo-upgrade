44b3308854925fe2f7f7f70d3e6479d7f4a9b0fd
Francois Blackburn <fblackburn@wazo.community>
2023-04-18 14:11:31 -0400

    remove invalid option for shell interpreter

--- pre-start.d/10-run-wazo-pg-config-update.sh
+++ pre-start.d/10-run-wazo-pg-config-update.sh
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 set -e
 set -u  # fail if variable is undefined
-set -o pipefail  # fail if command before pipe fails
 
 echo "Updating PostgreSQL config"
 
