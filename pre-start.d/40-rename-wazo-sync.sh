4917d7a5cdfd3c227b66bc49661f81da3256a9d0
Pascal Cadotte Michaud <pcm@wazo.io>
2024-01-09 11:08:31 -0500

    add a migration script for the ha cron

---
+++ pre-start.d/40-rename-wazo-sync.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright 2024 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -e
+set -u  # fail if variable is undefined
+set -o pipefail  # fail if command before pipe fails
+
+SENTINEL='/var/lib/wazo-upgrade/rename-sync-cron'
+CRON_FILENAME='/etc/cron.d/xivo-ha-master'
+
+if [ -e ${SENTINEL} ]; then
+    exit 0
+fi
+
+if [ ! -e ${CRON_FILENAME} ]; then
+    exit 0
+fi
+
+sed -i 's/xivo-sync/wazo-sync/g' $CRON_FILENAME
+touch $SENTINEL
