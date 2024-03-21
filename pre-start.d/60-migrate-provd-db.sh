ee5fcb4b8f18b3692d5bfc1a96b3de06477d3cd4
Francois Blackburn <fblackburn@wazo.community>
2024-03-21 15:03:00 -0400

    move logic to remove jsondb inside migration script
    why: the ownership of jsondb is wazo-provd

--- pre-start.d/60-migrate-provd-db.sh
+++ pre-start.d/60-migrate-provd-db.sh
@@ -2,33 +2,25 @@
 # Copyright 2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-set -e
+# set -e  # logic is based on return code
 set -u  # fail if variable is undefined
 set -o pipefail  # fail if command before pipe fails
 
 SENTINEL_MIGRATE="/var/lib/wazo-upgrade/migration-provd-db-data"
-SENTINEL_CLEANUP="/var/lib/wazo-upgrade/migration-provd-db-cleanup"
 
 # wazo-provd-migrate-db return code:
 # 0: Everything OK
 # 2: Migration already done
 
-if [ ! -e "${SENTINEL_MIGRATE}" ]; then
-    shopt -qu -o errexit  # logic is based on return code
-    wazo-provd-migrate-db
-    rc=$?
-    shopt -qs -o errexit
-
-    if [ "$rc" -eq 0 -o "$rc" -eq 2 ]; then
-        touch "${SENTINEL_MIGRATE}"
-    else
-        exit $rc
-    fi
+if [ -e "${SENTINEL_MIGRATE}" ]; then
+    exit
 fi
 
-if [ ! -e "${SENTINEL_CLEANUP}" ]; then
-    # Daily backup should take care of data backup
-    rm -f /var/lib/wazo-provd/app.json
-    rm -rf /var/lib/wazo-provd/jsondb
-    touch "${SENTINEL_CLEANUP}"
+wazo-provd-migrate-db
+rc=$?
+
+if [ "$rc" -eq 0 -o "$rc" -eq 2 ]; then
+    touch "${SENTINEL_MIGRATE}"
+else
+    exit $rc
 fi
