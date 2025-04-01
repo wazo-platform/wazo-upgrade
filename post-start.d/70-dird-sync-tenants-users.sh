3dd288d395aa5f1853222ec20107624919be375a
Alexandre Fournier <afournier@wazo.io>
2025-04-01 16:28:50 -0400

    post-start: add dird users upgrade script

---
+++ post-start.d/70-dird-sync-tenants-users.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Copyright 2025 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -e
+set -u  # fail if variable is undefined
+set -o pipefail  # fail if command before pipe fails
+
+SENTINEL="/var/lib/wazo-upgrade/dird-user-sync-tenant"
+if [[ -f $SENTINEL ]]; then
+    exit
+fi
+TOKEN=$(wazo-auth-cli token create)
+wazo-confd-cli --token $TOKEN user list -c uuid -c tenant_uuid --recurse -fjson --noindent | wazo-dird-user-sync
+touch $SENTINEL
